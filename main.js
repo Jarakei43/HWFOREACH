0fa8aa40f2e9a8be3fced30f5036463c5baa3576
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2024-10-28 17:48:50 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// N1
+const str = ["яблоко", "банан", "апельсин"];
+str.forEach((item) => {
+    console.log(item);
+});
+
+// N2
+// const arr = [1, 2, 3]
+
+// arr.forEach((item) => {
+//   console.log(item * 2);
+// })
+
+// N3
+// const num = [5, 10, 15];
+// let sum = 0
+
+// num.forEach((item) => {
+//   console.log((sum += item));
+// });
+
+// N4
+// const str =  ['привет', 'мир']
+
+// str.forEach((item) => {
+//   console.log(item.toUpperCase(item))
+// })
+
+// N5
+// const num2 = [2, 5, 8, 1, 10];
+// let isBigger = [];
+
+// num2.forEach((item) => {
+//   if (item > 5) {
+//     isBigger.push(item);
+//     console.log(isBigger);
+//   }
+// });
